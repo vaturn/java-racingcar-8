e2bfc767a24e26ead37ec1d3ccc5a4ca4c484c76
vaturn <will6867@naver.com>
2025-10-27 22:50:38 +0900

    feat: 이름 중복 발생시 오류 반환 기능 추기

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -3,7 +3,9 @@ package racingcar.controller;
 import racingcar.model.Car;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 record CarStatus(String name, int distance) {}
@@ -13,6 +15,9 @@ public class CarController {
     private static final int MINIMUM_POSSIBLE = -1;
 
     public void createCarList(List<String> carNameList){
+        if(hasDuplicates(carNameList))
+            throw new IllegalArgumentException();
+
         for(String name : carNameList){
             carList.add(new Car(name));
         }
@@ -36,5 +41,11 @@ public class CarController {
         int maxDistance = carList.stream().mapToInt(Car::getDistance).max().orElse(MINIMUM_POSSIBLE);
         List<String> winners = carList.stream().filter(car -> car.getDistance() == maxDistance).map(Car::getCarName).collect(Collectors.toList());
         return winners;
+    }
+
+    private boolean hasDuplicates(List<String> nameList){
+        Set<String> nameDistinct = new HashSet<>(nameList);
+
+        return nameList.size() > nameDistinct.size();
     }
 }
