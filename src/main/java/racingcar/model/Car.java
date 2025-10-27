caa6a8928a46c52370affa864299fb3a96ab1734
vaturn <will6867@naver.com>
2025-10-27 18:44:55 +0900

    feat: 0~9 중 4이상인 경우에 차가 한칸 움직이는 기능 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,13 +1,26 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String carName;
-    private final int distance;
+    private int distance;
     private static final int UNIT_DISTANCE = 1;
+    private static final int MAX_RANDOM_BOUND = 9;
+    private static final int MIN_RANDOM_BOUND = 0;
+    private static final int ACTION_THRESHOLD = 4;
 
     public Car(String carName){
         this.carName = carName;
         distance = 0;
+    }
+
+    private void move(){
+        int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_BOUND, MAX_RANDOM_BOUND);
+
+        if(randomNumber >= ACTION_THRESHOLD){
+            this.distance += UNIT_DISTANCE;
+        }
     }
 
     public String getCarName() {
