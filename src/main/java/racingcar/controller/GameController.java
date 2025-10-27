10d2f8df847c9708cf6694e2d94ddb1b95a65084
vaturn <will6867@naver.com>
2025-10-27 18:33:38 +0900

    feat: 사용자가 입력한 이름 목록에 따라 자동차 객체 생성하는 기능 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,9 +1,13 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
     private final ConsoleController consoleController;
+    private final List<Car> carList = new ArrayList<>();
 
     public GameController(){
         consoleController = new ConsoleController();
@@ -12,9 +16,17 @@ public class GameController {
     public void runGame(){
         List<String> carNameList =  consoleController.progressInputCarNamePhase();
 
+        createCarList(carNameList);
+
         int stepOfNumber = consoleController.progressInputNumberPhase();
 
         System.out.println(carNameList);
         System.out.println(stepOfNumber);
+    }
+
+    private void createCarList(List<String> carNameList){
+        for(String name : carNameList){
+            carList.add(new Car(name));
+        }
     }
 }
